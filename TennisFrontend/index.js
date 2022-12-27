d3f05edbff356b6b9b7eef4a8dea9e2897cb933d
Jad Shaheen <shaheenjad@gmail.com>
2022-12-26 19:51:41 -0500

    Adding alerting to tennis UI when servers are down for user transparency

--- TennisFrontend/index.js
+++ TennisFrontend/index.js
@@ -7,48 +7,58 @@ function getPlayerInfo() {
     let url_with_params = url + "?" + new URLSearchParams({
         query: searchQuery,
     })
+    let data = null;
     fetch(url_with_params)
-        .then(resp => resp.json())
-        .then(data => {
-            let htmlString = "";
-            if (data['player']) {
-                let player = data['player'];
-                if (player['rank']) {
-                    htmlString += "<p>At age " + player['age'] +
-                        ", " + normalizeString(searchQuery) +
-                        " is currently ranked number " +
-                        "<b>" + player['rank'] + "</b>" +
-                        " in the world by the ATP.</p>";
-                }
-                // htmlString += "<p>He has won " + player -- FIGURE OUT HOW TO SAY X WINS OUT OF Y FINALS APPEARANCES HERE
-                htmlString += "<p>Here are <b>" + normalizeString(searchQuery.toLowerCase()) + "</b>'s results in Grand Slam finals:</p>";
-                if (Object.keys(player['tournaments']).length > 0) {
-                    htmlString += buildPlayerTournamentsTable(player['tournaments'])
-                }
-
-            } else if (data['tournament']) {
-                let tournament = data['tournament'];
-                htmlString += "<p>Here are the results of the <b>" + normalizeString(searchQuery.toLowerCase()) + "</b> final by year:</p>";
-                htmlString += buildFinalistsTable(tournament, 'tournament');
-            } else if (data['year']) {
-                let year = data['year'];
-                htmlString += "<p>Here are the results of the <b>" + searchQuery + "</b> Grand Slam finals:</p>";
-                htmlString += buildFinalistsTable(year, 'year');
-            } else if (data['rankings']) {
-                let rankings = data['rankings'];
-                let mensRankings = rankings[0]
-                let womensRankings = rankings[1]
-                htmlString += "<div class='ranktable'>"
-                htmlString += "<p>Current <b>ATP Men's Singles</b> rankings:</p>";
-                htmlString += buildRankingsTable(mensRankings);
-                htmlString += "</div>"
-                htmlString += "<div class='ranktable'>"
-                htmlString += "<p>Current <b>ATP Women's Singles</b> rankings:</p>";
-                htmlString += buildRankingsTable(womensRankings);
-                htmlString += "</div>"
+        .then(
+            resp => {
+                data = resp.json();
+            },
+            error => {
+                console.log("PROMISE FAILURE!");
+                alert("ERROR (503): The application servers are unavailable.");
             }
-            playerDataDiv.innerHTML = htmlString;
-        })
+        )
+
+    if (data != null) {
+        let htmlString = "";
+        if (data['player']) {
+            let player = data['player'];
+            if (player['rank']) {
+                htmlString += "<p>At age " + player['age'] +
+                    ", " + normalizeString(searchQuery) +
+                    " is currently ranked number " +
+                    "<b>" + player['rank'] + "</b>" +
+                    " in the world by the ATP.</p>";
+            }
+            // htmlString += "<p>He has won " + player -- FIGURE OUT HOW TO SAY X WINS OUT OF Y FINALS APPEARANCES HERE
+            htmlString += "<p>Here are <b>" + normalizeString(searchQuery.toLowerCase()) + "</b>'s results in Grand Slam finals:</p>";
+            if (Object.keys(player['tournaments']).length > 0) {
+                htmlString += buildPlayerTournamentsTable(player['tournaments'])
+            }
+
+        } else if (data['tournament']) {
+            let tournament = data['tournament'];
+            htmlString += "<p>Here are the results of the <b>" + normalizeString(searchQuery.toLowerCase()) + "</b> final by year:</p>";
+            htmlString += buildFinalistsTable(tournament, 'tournament');
+        } else if (data['year']) {
+            let year = data['year'];
+            htmlString += "<p>Here are the results of the <b>" + searchQuery + "</b> Grand Slam finals:</p>";
+            htmlString += buildFinalistsTable(year, 'year');
+        } else if (data['rankings']) {
+            let rankings = data['rankings'];
+            let mensRankings = rankings[0]
+            let womensRankings = rankings[1]
+            htmlString += "<div class='ranktable'>"
+            htmlString += "<p>Current <b>ATP Men's Singles</b> rankings:</p>";
+            htmlString += buildRankingsTable(mensRankings);
+            htmlString += "</div>"
+            htmlString += "<div class='ranktable'>"
+            htmlString += "<p>Current <b>ATP Women's Singles</b> rankings:</p>";
+            htmlString += buildRankingsTable(womensRankings);
+            htmlString += "</div>"
+        }
+        playerDataDiv.innerHTML = htmlString;
+    }
 }
 
 function buildPlayerTournamentsTable(tournamentsData) {
